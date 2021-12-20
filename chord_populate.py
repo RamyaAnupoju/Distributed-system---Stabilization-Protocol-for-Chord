44755f52f1e5a60941736c9c880438f5f1c22ac4
RamyaAnupoju <79663440+RamyaAnupoju@users.noreply.github.com>
2021-12-20 11:01:00 -0800

    Implemented logic to populate data in chord_populate.py

---
+++ chord_populate.py
@@ -0,0 +1,35 @@
+import csv
+import socket
+import pickle
+import json
+import time
+import sys
+class chord_populate:
+    def main(self,port,filePath):
+        ip = "127.0.0.1"
+        port = int(port)
+        with open(filePath, mode='r') as csv_file:
+            """
+            Reads player data from csv
+            """
+            csv_reader = csv.DictReader(csv_file)
+            line_count = 1
+            for row in csv_reader:
+                sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                sock.connect((ip,port))
+                print(f'\t{row["Player Id"]} - {row["Year"]}.')
+                key = row["Player Id"] + "_" + row["Year"]
+                val = json.dumps(row)
+                message = "insert|" + str(key) + ":" + str(val)
+                sock.sendall(pickle.dumps(message))
+                line_count += 1
+                sock.close()
+                time.sleep(1)
+            print(f'completed {line_count} records.')
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        chord_populate = chord_populate()
+        chord_populate.main(sys.argv[1],sys.argv[2])
+    else:
+         print("Please provide port & file path as args")
